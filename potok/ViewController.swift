aecb9e95347fce29ca3bcc3e36951a8a638fcae4
duhesme <hitechwatchout@gmail.com>
2022-05-31 18:12:04 +0300

    Initial commit.

---
+++ potok/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  potok
+//
+//  Created by Никита Владимирович on 31.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
